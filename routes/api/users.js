360fe0ec63ecc43df554c79297862575a0af2abd
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-01-20 04:31:30 -0800

    Update register user API

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,23 +4,34 @@ const router = express.Router();
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
+
 router.post("/register", (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
+  // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      // Use the validations to send the error
-      errors.email = "Email already exists";
-      return res.status(400).json(errors);
+      // Throw a 400 error if the email address already exists
+      return res
+        .status(400)
+        .json({ email: "A user has already registered with this address" });
     } else {
+      // Otherwise create a new user
       const newUser = new User({
-        name: req.body.name,
+        handle: req.body.handle,
         email: req.body.email,
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
