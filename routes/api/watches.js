3ded4623e85b1b6d9a4c8a0d18efa09ace6f386a
Abel Chiao <abelchiao@Abels-MacBook-Pro.local>
2020-01-21 11:25:40 -0800

    Add search route skeleton

--- routes/api/watches.js
+++ routes/api/watches.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Watch = require('../../models/Watch');
 
+// all watches
 router.get('/', (req, res) => {
   Watch.find()
     .then(watches => res.json(watches))
     .catch(err => res.status(404).json({ nowatchesfound: "No watches found" }));
 });
 
+// single watch by id
+router.get('/:id', (req, res) => {
+  Watch.findById(req.params.id)
+    .then(watch => res.json(watch))
+    .catch(err => res.status(404).json({ nowatchfound: 'No watch found with that Id'}))
+});
+
+// create new watch
 router.post('/', (req, res) => {
   const newWatch = new Watch({
     brand: req.body.brand,
@@ -21,4 +30,17 @@ router.post('/', (req, res) => {
   }
 );
 
+router.post("/search", (req, res) => {
+  let brand, model, price
+  if (req.body.brand) brand = req.body.brand
+  if (req.body.model) model = req.body.model
+  if (req.body.price) price = req.body.price
+
+  Watch.find({
+    brand: brand, model: model, price: price
+  })
+  // Watch.find()
+    .then(watches => res.json(watches))
+    .catch(err => res.status(404).json({ nowatchesfound: "No watches found" }));
+});
 module.exports = router;
