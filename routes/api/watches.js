840d26583f0bdc64c67e74ddfd4f7970cedd6881
Abel Chiao <abelchiao@Abels-MacBook-Pro.local>
2020-01-21 11:59:20 -0800

    Add ability to search with multiple optional fields

--- routes/api/watches.js
+++ routes/api/watches.js
@@ -31,15 +31,40 @@ router.post('/', (req, res) => {
 );
 
 router.post("/search", (req, res) => {
-  let brand, model, price
-  if (req.body.brand) brand = req.body.brand
-  if (req.body.model) model = req.body.model
-  if (req.body.price) price = req.body.price
+  // let brand, model, price
+  // let watches = Watch.find()
+  // if (req.body.brand) {
+  //   brand = req.body.brand
+  //   watches = watches.find({ brand: brand })
+  // }
+  // if (req.body.model) {
+  //   model = req.body.model
+  //   watches = watches.find({ model: model })
+  // }
+  // if (req.body.price) {
+  //   price = req.body.price
+  //   watches = watches.find({ price: price })
+  // }
+  // watches => res.json(watches)
+  //   .catch(err => res.status(404).json({ nowatchesfound: "No watches found" }));
 
-  Watch.find({
-    brand: brand, model: model, price: price
-  })
-  // Watch.find()
+  // let brand, model, price
+  // if (req.body.brand) brand = req.body.brand;
+  // if (req.body.model) model = req.body.model;
+  // if (req.body.price) price = req.body.price;
+
+  // Watch.find({
+  //   brand: brand, model: model, price: price
+  // })
+  //   .then(watches => res.json(watches))
+  //   .catch(err => res.status(404).json({ nowatchesfound: "No watches found" }));
+  
+  let query = {}
+  if (req.body.brand) query.brand = req.body.brand;
+  if (req.body.model) query.model = req.body.model;
+  if (req.body.price) query.price = req.body.price;
+  console.log(query)
+  Watch.find( query )
     .then(watches => res.json(watches))
     .catch(err => res.status(404).json({ nowatchesfound: "No watches found" }));
 });
