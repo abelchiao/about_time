0cb9eddafec980e39b529f7e5620791abb9bb134
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-01-21 19:20:44 -0800

    Add additional npm packages to frontend

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
-const path = require('path');
+// const path = require('path');
 
 import MainPage from './main/main_page';
 import SearchFormContainer from './search/search_container';
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('frontend/build'));
+//     app.get('/', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//     })
+// }
 const App = () => (
     <div>
         <Switch>
