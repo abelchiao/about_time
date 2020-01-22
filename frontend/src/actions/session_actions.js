4cd6b008e3e905c4720ef865e0fbf4b0ce60089c
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-01-21 19:01:23 -0800

    Remove duplicate logoutUser method from session_actions

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -6,9 +6,9 @@ export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 
-export const logoutUser = () => ({
-    type: RECEIVE_USER_LOGOUT
-});
+// export const logoutUser = () => ({
+//     type: RECEIVE_USER_LOGOUT
+// });
 
 export const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER,
