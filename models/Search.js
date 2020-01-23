e7b1ed4d657bbc53c846f7b9867d58aa4c976583
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-01-23 11:23:13 -0800

    Update user and search models

--- models/Search.js
+++ models/Search.js
@@ -2,17 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const SearchSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users',
-    required: true
-  },
+  // user: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: "users",
+  //   required: true
+  // },
   date: {
     type: Date,
     default: Date.now
   },
   label: {
     type: String,
+    required: true
+  },
+  parameters: {
+    type: Object,
     required: true
   }
 });
