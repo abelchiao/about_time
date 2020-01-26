7b7f59d4fdf7375291c6d196ec3b04d4630e9f49
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-01-26 06:10:14 -0800

    Add watches seed file via line reader

--- app.js
+++ app.js
@@ -29,16 +29,111 @@ mongoose.set('useFindAndModify', false);
 
 
 
-var promise = Watch.create({
-  brand: "testBrand",
-  model: "testModel",
-  style: "testStyle",
-  price: 123,
-  reviews: {
-    userId: "5e2d27c5b2da08467f469ca4",
-    text: "testText"
-  }
+// var promise = Watch.create({
+//   brand: "testBrand",
+//   model: "testModel",
+//   style: "testStyle",
+//   price: 123,
+//   reviews: {
+//     userId: "5e2d27c5b2da08467f469ca4",
+//     text: "testText"
+//   }
+// });
+
+// const fs = require("fs");
+// const itemResult = await Watch.find({}).exec();
+// if(itemResult.length !== 0) {
+
+//   const itemsSeedDataRaw = fs.readFileSync(
+//     `${__dirname}/data/watchesSeedRevArrMinAddLine2.json`,
+//     "utf8"
+//   );
+// console.log(itemsSeedDataRaw)
+//   const itemsSeedData = JSON.parse(itemsSeedDataRaw);
+//   Watch.insertMany(itemsSeedData);
+
+  // await Watch.insertMany(itemsSeedData);
+
+// }
+
+
+// fs.readFile("./data/watchesSeedRevArrMin.json", (err, data) => {
+//   if (err) throw err;
+//   let watches = JSON.parse(data);
+//   console.log(watches);
+// });
+
+
+// let jsonData = require("./data/watchesSeedRevArrMin.json");
+// console.log(jsonData);
+
+
+// const fs = require("fs");
+// const fileContents = fs.readFileSync(
+//   "./data/watchesSeedRevArrMin.json",
+//   "utf8"
+// );
+// console.log(fileContents)
+// try {
+//   const data = JSON.parse(fileContents);
+// } catch (err) {
+//   console.error(err);
+// }
+
+
+// const fs = require("fs");
+
+// fs.readFile("/path/to/file.json", "utf8", (err, fileContents) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   try {
+//     const data = JSON.parse(fileContents);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// });
+
+
+
+
+// const csvParser = require("csv-parser");
+// // const fs = require("fs");
+
+// const filepath = "./data/watchesSeedRevArrMinAddLine2.json";
+
+// fs.createReadStream(filepath)
+//   .on("error", err => {
+//     // handle error
+//     console.log(err);
+//   })
+
+//   // .pipe(csvParser())
+//   JSON.parse()
+//   .on("data", row => {
+//     // use row data
+//     console.log(row);
+
+//     Watch.create(row);
+//   })
+
+//   .on("end", () => {
+//     // handle end of CSV
+//   });
+
+
+
+
+const lineReader = require("line-reader");
+let itemsSeedData;
+lineReader.eachLine("./data/watchesSeedRevArrMinAddLine2.json", function(line) {
+  console.log(line);
+  itemsSeedData = JSON.parse(line);
+  Watch.insertMany(itemsSeedData);
 });
+
+
 
 
 // app.get("/", (req, res) => res.send("Hello World0"));
