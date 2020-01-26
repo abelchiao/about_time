32eef635757109e09a277aed277b64c1764501ce
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-01-25 16:28:36 -0800

    Update backend review routes with error check and notes for frontend

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -57,14 +57,16 @@ router.post("/", passport.authenticate("jwt", { session: false }), (req, res) =>
   Watch.findById(req.body.watchId, (err, watch) => {
     let reviews = watch.reviews;
     let error
-    reviews.forEach(review => {
-      console.log(review)
-      if (review.userId.toString() === req.user.id) {
-        error = "You have already reviewed this watch"
-        // return res.status(403).json("You have already reviewed this watch");
-      }
-    });
-    if (error) return res.status(403).json(error);
+    if (reviews.length !== 0) {
+      reviews.forEach(review => {
+        console.log(review)
+        if ((review.length !== 0) && (review.userId.toString() === req.user.id)) {
+          error = "You have already reviewed this watch"
+          // return res.status(403).json("You have already reviewed this watch");
+        }
+      });
+      if (error) return res.status(403).json(error);
+    }
     // need to coordinate what req.body will look like w/ FE
     let newReview = ({
       userId: req.user.id,
@@ -98,6 +100,10 @@ router.post("/", passport.authenticate("jwt", { session: false }), (req, res) =>
 router.post("/edit", passport.authenticate("jwt", { session: false }), (req, res) => {
   Watch.findById(req.body.watchId, (err, watch) => {
     let review = watch.reviews.id(req.body.reviewId);
+
+    // TODO via postman any user can delete any review
+    // so frontend should limit edit button to appear only on review which belongs to logged in user
+
     review.set({
       // userId: req.user.id,
       text: req.body.text
@@ -116,6 +122,11 @@ router.delete(
     Watch.findById(req.body.watchId, (err, watch) => {
 
       // TODO match code from search delete route if review object id is not passed in
+      
+      // TODO pull will silently fail/noop if reviewId not found in watch.reviews
+
+      // TODO via postman any user can delete any review
+      // so frontend should limit delete button to appear only on review which belongs to logged in user
 
       watch.reviews.pull(req.body.reviewId);
 
