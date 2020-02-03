c6b95f52cc3275082ffe3381b169efce3c7c42b3
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-02-03 14:44:01 -0800

    Add initial profile page

--- frontend/src/components/users/profile_page.js
+++ frontend/src/components/users/profile_page.js
@@ -5,28 +5,46 @@ class ProfilePage extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    if (this.props.currentUser.handle) {
+        console.log("handle: ", this.props.currentUser.handle);
+        let tmp = this.props.fetchUserSearches();
+        console.log("tmp: ", tmp)
+    }
+  }
+
   render() {
-    console.log(this.props);
+    console.log("PROPS: ", this.props);
+    console.log("STATE: ", this.state);
+    
+    if (!this.props.currentUser.handle) {
+        return (
+            <div className="profile-page-container">
+                Please log in to see your profile page.
+            </div>
+        )
+    } else {
+        return (
+          <div className="profile-page-container">
+            <p>Welcome, {this.props.currentUser.handle}:</p>
 
-    if (!this.props.currentUser.handle) return (
-        <div>
-            Please log in to see your profile page.
-        </div>
-    );
-
-    return (
-      <div className="profile-page-container">
-
-        <p>Welcome, {this.props.currentUser.handle}:</p>
-
-      </div>
-    );
+            <div className="search-items-container">
+              {
+            //   console.log(this.props.searches.all[0].label)
+              // this.props.searches.all[0].label
+              // this.props.searches.all.map( search => {
+              //     return search.label
+              // })
+              }
+            </div>
+          </div>
+        );
+    }
 
   }
 }
 
 export default ProfilePage;
-
 
 
 
--- frontend/src/components/users/profile_page_container.js
+++ frontend/src/components/users/profile_page_container.js
@@ -1,10 +1,15 @@
 import { connect } from "react-redux";
 import ProfilePage from "./profile_page";
+import { fetchUserSearches } from '../../actions/search_actions';
+import { STATES } from "mongoose";
 
 const mapStateToProps = state => ({
-    currentUser: state.session.user
+    currentUser: state.session.user,
+    searches: state.searches
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  fetchUserSearches: () => dispatch(fetchUserSearches())
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
