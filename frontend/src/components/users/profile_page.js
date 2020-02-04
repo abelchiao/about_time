180280be55216a6180284f4a89c97a8a3ce5b2c8
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-02-03 18:54:01 -0800

    Update profile page to display handles and saved search labels for logged in user

--- frontend/src/components/users/profile_page.js
+++ frontend/src/components/users/profile_page.js
@@ -3,44 +3,55 @@ import React from "react";
 class ProfilePage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        loaded: false
+    };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     if (this.props.currentUser.handle) {
-        console.log("handle: ", this.props.currentUser.handle);
-        let tmp = this.props.fetchUserSearches();
-        console.log("tmp: ", tmp)
+        await this.props.fetchUserSearches()
+        this.setState({loaded: true});
     }
   }
 
   render() {
-    console.log("PROPS: ", this.props);
     console.log("STATE: ", this.state);
-    
-    if (!this.props.currentUser.handle) {
-        return (
-            <div className="profile-page-container">
-                Please log in to see your profile page.
-            </div>
-        )
+    console.log("PROPS: ", this.props);
+
+    if (this.state.loaded === false) {
+        // console.log("FALSE STATE TRIGGER")
+        return null
     } else {
-        return (
-          <div className="profile-page-container">
-            <p>Welcome, {this.props.currentUser.handle}:</p>
 
-            <div className="search-items-container">
-              {
-            //   console.log(this.props.searches.all[0].label)
-              // this.props.searches.all[0].label
-              // this.props.searches.all.map( search => {
-              //     return search.label
-              // })
-              }
+        let searches;
+        if ((this.props.searches.all.length === undefined) || (this.props.searches.all.length === 0)) {
+            searches = <div>No saved searches</div>
+        } else {
+            searches = this.props.searches.all.map( (search, idx) => {
+                    return <p key={idx}>{search.label}</p>
+                })
+        }
+
+        if (!this.props.currentUser.handle) {
+            return (
+                <div className="profile-page-container">
+                    Please log in to see your profile page.
+                </div>
+            )
+        } else {
+            return (
+            <div className="profile-page-container">
+                <p>Welcome, {this.props.currentUser.handle}:</p>
+
+                <div className="search-items-container">
+                    { searches }
+                </div>
             </div>
-          </div>
-        );
-    }
+            );
+        }
 
+    }
   }
 }
 
@@ -48,8 +59,11 @@ export default ProfilePage;
 
 
 
-// TODO add saved searches to global state OR pull searches from DB on open of profile page
+// DONE TODO add saved searches to global state OR pull searches from DB on open of profile page
 // TODO iterate across reviews for all watches to show any from the user
-// TODO profile page should reflect only logged in OR any user
+// DONE TODO profile page should reflect only logged in OR any user
+// TODO add link from search label to hit search api
+// TODO add query parameters to search label
+// DONE TODO add check for data loaded before render
 
 
