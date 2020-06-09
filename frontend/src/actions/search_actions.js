0b35cf76b2de31a47260973256d97c72d6835500
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-06-09 11:10:01 -0700

    Update error handling for search action

--- frontend/src/actions/search_actions.js
+++ frontend/src/actions/search_actions.js
@@ -7,22 +7,22 @@ export const RECEIVE_USER_SEARCHES = "RECEIVE_USER_SEARCHES";
 export const RECEIVE_SEARCH_ERRORS = "RECEIVE_SEARCH_ERRORS";
 export const REMOVE_SEARCH = "REMOVE_SEARCH";
 
-export const receiveSearch = (search) => ({
+const receiveSearch = (search) => ({
     type: RECEIVE_SEARCH,
     search
 });
 
-export const receiveNewSearch = (search) => ({
+const receiveNewSearch = (search) => ({
     type: RECEIVE_NEW_SEARCH,
     search
 });
 
-export const receiveUserSearches = (searches) => ({
+const receiveUserSearches = (searches) => ({
     type: RECEIVE_USER_SEARCHES,
     searches
 });
 
-export const receiveErrors = errors => ({
+const receiveErrors = errors => ({
     type: RECEIVE_SEARCH_ERRORS,
     errors
 });
@@ -34,25 +34,25 @@ const removeSearch = searchId => ({
 
 export const fetchUserSearches = () => dispatch => (
     SearchApiUtil.getUserSearches()
-        .then(searches => dispatch(receiveUserSearches(searches)))
-        .catch(err => console.log(err))
+        .then( searches => dispatch(receiveUserSearches(searches)) )
+        .catch( err => dispatch(receiveErrors(err)) )
 );
 
 // subdoc - don't need this action - not tested
-export const fetchSearch = (data) => dispatch => (
+export const fetchSearch = data => dispatch => (
     SearchApiUtil.getSearch(data)
-        .then(search => dispatch(receiveSearch(search)))
-        .catch(err => console.log(err))
+        .then( search => dispatch(receiveSearch(search)) )
+        .catch( err => dispatch(receiveErrors(err)) )
 );
 
-export const newSearch = (data) => dispatch => (
+export const newSearch = data => dispatch => (
     SearchApiUtil.createSearch(data)
-        .then(search => dispatch(receiveNewSearch(search)))
-        .catch(err => console.log(err))
+        .then( search => dispatch(receiveNewSearch(search)) )
+        .catch( err => dispatch(receiveErrors(err)) )
 );
 
 export const deleteSearch = searchId => dispatch => (
     SearchApiUtil.deleteSearch(searchId)
-        .then(() => dispatch(removeSearch(searchId)))
-        .catch(err => console.log(err))
+        .then( () => dispatch(removeSearch(searchId)) )
+        .catch( err => dispatch(receiveErrors(err)) )
 );
