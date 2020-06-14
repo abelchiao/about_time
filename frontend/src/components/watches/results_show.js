997a016c55bfb9bceddad80a0d7d840ba2858345
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-06-13 21:43:18 -0700

    Update layout and css for results show component

--- frontend/src/components/watches/results_show.js
+++ frontend/src/components/watches/results_show.js
@@ -170,18 +170,18 @@ class ResultsShow extends React.Component {
             <div className="results-background">
                 <Navbar />
                 <div className="search-result-show">
+                    <div className="search-result-nav-container">
+                        { saveSearchInputs }
+                    </div>
                     <div className="search-result-query-tags">
                         <ul className="search-result-query-tags-list">
                             { searchQueryTags }
                         </ul>
                     </div>
-                    <div className="search-result-nav-container">
-                        { saveSearchInputs }
-                    </div>
                     <div className="search-result-query-tags-sort">
                         <div className="search-result-query-tags-list-sort-button" onClick={ () => this.handleShowSort() }>
                             <div className="search-result-query-tags-list-item-text">
-                                SORT
+                                SORT RESULTS
                             </div>
                             <div className="search-result-query-tags-list-item-sort">
                                 <svg className="search-result-query-tags-list-item-sort-icon-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-double-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"></path></svg>
