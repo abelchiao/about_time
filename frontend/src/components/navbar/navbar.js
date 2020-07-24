8da300414fc6775d17464a7bb6a5beba553aed07
Jaydip Dalal <jaydip.dalal@gmail.com>
2020-07-24 05:02:22 -0700

    Add initial help popup for navbar component

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -34,6 +34,7 @@ class Navbar extends React.Component {
     render() {
         window.onclick = function (event) {
             event.stopPropagation();
+console.log("HERE111")
             if (!event.target.matches('.dropbtn')) {
                 let dropdowns = document.getElementsByClassName("dropdown-content");
                 let i;
@@ -50,6 +51,11 @@ class Navbar extends React.Component {
             if (alert !== undefined) {
                 alert.style.display = (alert.style.display !== "none") ? "none" : alert.style.display;
             };
+
+            let help = document.getElementsByClassName("help")[0];
+            if (help !== undefined) {
+                help.style.display = (help.style.display !== "none") ? "none" : help.style.display;
+            };
         };
 
         return (
@@ -59,6 +65,22 @@ class Navbar extends React.Component {
                     <span className="alert-close">
                         <svg className="alert-close-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
                     </span>
+                </div>
+                <div className="help" >
+                    <div className="help-header">
+                        <div className="help-text">SOME HELPFUL HINTS</div>
+                        <span className="help-close">
+                            <svg className="help-close-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+                        </span>
+                    </div>
+                    <div className="help-list">
+                        <div>TO SEARCH FOR WATCHES:</div>
+                        <ul>
+                            <li><div>1) PICK ANY OPTIONS, THEN CLICK SEARCH</div></li>
+                            <li><div>2) SAVE SEARCHES BY ENTERING A LABEL</div></li>
+                            <li><div>3) VIEW SAVED SEARCHES FROM THE PROFILE PAGE</div></li>
+                        </ul>
+                    </div>
                 </div>
                 <button onClick={ e  => this.toggleDropdown(e) } className="dropbtn">
                     <svg aria-hidden="true" onClick={ e => this.toggleDropdown(e) } className="dropbtn-svg" focusable="false" data-prefix="fas" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
@@ -70,11 +92,13 @@ class Navbar extends React.Component {
                             <div>
                                 <button className="dropdown-item" onClick={ this.openLoginModal }>LOG IN</button>
                                 <button className="dropdown-item" onClick={ this.openSignupModal }>SIGN UP</button>
+                                <button className="dropdown-item" onClick={(e) => { e.stopPropagation(); document.getElementsByClassName("help")[0].style.display = "flex"}}>HELP</button>
                                 { (window.location.hash !== "#/") ? <button className="dropdown-item" onClick={ () => this.props.history.push(`/`) }>NEW SEARCH</button> : null }
                             </div>
                         : 
                             <div>
                                 <button className="dropdown-item" onClick={this.logout}>SIGN OUT</button>
+                                <button className="dropdown-item" onClick={ (e) => { e.stopPropagation(); document.getElementsByClassName("help")[0].style.display = "flex"}}>HELP</button>
                                 { (window.location.hash !== "#/profile") ? <button className="dropdown-item" onClick={ () => this.props.history.push(`/profile`) }>PROFILE</button> : null }
                                 { (window.location.hash !== "#/") ? <button className="dropdown-item" onClick={ () => this.props.history.push(`/`) }>NEW SEARCH</button> : null }
                             </div>
